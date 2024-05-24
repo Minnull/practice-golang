f3e0d29dc59706916f2466cf8d7cfe603a5a1dec
Mainnull <Mainnull>
2024-05-24 16:20:39 +0800

    add grpc stream send and recv

--- 02_advanced/02_grpc/02_stream/server/main.go
+++ 02_advanced/02_grpc/02_stream/server/main.go
@@ -20,9 +20,22 @@ type GreeterServer struct {
 // 服务端 单向流
 func (s *GreeterServer) GetStream(req *pro.StreamReqData, res pro.Greeter_GetStreamServer) error {
 	i := 0
+	log.Println("外部开始执行", i)
 	for {
 		i++
-		res.Send(&pro.StreamResData{Data: fmt.Sprintf("%v", time.Now().Unix())})
+		log.Println("开始发送", i)
+		// sleep可以减速，观察现象
+		// 不管加sleep还是不加sleep：测试重启client，server不会立即停止发送，而是继续发送一段时间后停止。client重启后，无法断点续传，
+		//
+		// server stream1发送的数据：1、2、3、4、【这时候client停止服务，server没有感知，继续发送后面的数据】、5、6、7、8、9、10
+		// server stream2发送的数据：1、2、3、4、5、6、7、8、9、10
+		// client接收的数据：1、2、3、4、【client停止服务，停止接收stream1流的数据，重启后，后面是stream2发送的数据】、1、2、3、4、5、6、7、8、9、10
+		//
+		// 总结：client关闭不会影响server的stream流停止发送数据，client新创建连接后，会新建stream流从头开始接收数据。
+		// 并且server的stream1、stream2流同时存在，继续发送数据，client只与最新的stream2保持数据同步
+		//time.Sleep(3 * time.Second)
+		res.Send(&pro.StreamResData{Data: fmt.Sprintf("%v %v", time.Now().Unix(), i)})
+		log.Println("发送完成", i)
 		time.Sleep(1 * time.Second)
 		if i > 10 {
 			break
